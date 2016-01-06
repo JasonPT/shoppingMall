b023ccb0bc1837effd387d14eebd037f87281119
alexanderpttt <alexanderpttt@gmail.com>
2016-01-06 11:43:32 -0600

    modify comment

--- models/ware.js
+++ models/ware.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 module.exports = mongoose.model('Ware',new mongoose.Schema({
-    name:String, //商品名称
-    price:Number,//商品价格
-    imgSrc:String, //商品图片路径
+    name:String,
+    price:Number,
+    imgSrc:String,
 }));
